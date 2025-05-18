e94370ffd07c2d3cefbadfc025a8882e4aa563ad
“Cooper Brady” <cbra207@wgu.edu>
2025-05-17 22:12:38 -0600

    I have been able to decide upon using FLASK as the foundation of building the CRUD APP, I build the rough foundation and have been able to connect enough of it to atleast add new info or items to page2 of the website, I will need to work on making the CRUD APP delete items next I think. Make a lot of progress though this week.

--- CRUD_FLASK/crud_app.py
+++ CRUD_FLASK/crud_app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request
 import pickle, time, os
 from flask_apscheduler import APScheduler
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 global dictionary
 dictionary = {}
 file = open("dictionary_file.pkl", 'wb')
 pickle.dump(dictionary, file)
 file.close()
+
 
 execute_clear_dictionary = APScheduler()
 
@@ -34,20 +38,45 @@ def add_put_elements():
 # Update Dictionary
 @app.route("/success", methods=['POST'])
 def success():
-    file = open("dictionary_file.pkl", 'rb')
-    dictionary = pickle.load(file)
-    file.close()
+    # Load dictionary from pickle
+    with open('dictionary_file.pkl', 'rb') as file:
+        dictionary = pickle.load(file)
 
-    html_data_1 = request.form["entry1"]
-    html_data_2 = request.form["entry2"]
-    dictionary[html_data_1] = html_data_2
+    # Get form fields
+    name = request.form['name']
+    breed = request.form['breed']
+    age = request.form['age']
+    pedigree = request.form['pedigree']
+    description = request.form['description']
 
-    file = open("dictionary_file.pkl", 'wb')
-    pickle.dump(dictionary, file)
-    file.close()
-    time.sleep(1)
+    #Handle image file(s)
+    file = request.files.get('image')
 
-    return render_template("/success.html", html_data_1=html_data_1, html_data_2=html_data_2)
+    if file and file.filename != '':
+    #Handle no file uploaded case
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    else:
+        filename = None
+
+
+    #Save all data including image filename
+    dictionary[name] = {
+        'image': filename,
+        'name': name,
+        'breed': breed,
+        'age': age,
+        'pedigree': pedigree,
+        'description': description
+    }
+
+    # Save back to pickle file
+    with open('dictionary_file.pkl', 'wb') as file:
+        pickle.dump(dictionary, file)
+
+    generate_animals_html(dictionary)
+
+    return render_template("/success.html")
 
 # GET
 @app.route("/get_page")
@@ -90,6 +119,64 @@ def clear_dictionary():
     file = open("dictionary_file.pkl", 'wb')
     pickle.dump(dictionary, file)
     file.close()
+
+# Path to the Brady's Farm website folder
+# Path to the Brady's Farm website folder
+def generate_animals_html(dictionary):
+    output_folder = os.path.join("..", "Brady's Farm")
+    output_path = os.path.join(output_folder, "page2.html")
+
+    # Make sure folder exists
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Convert dictionary to list if needed
+    if isinstance(dictionary, dict):
+        animals = list(dictionary.values())
+    else:
+        animals = dictionary
+
+    # Start of HTML content
+    html_content = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Brady's Farm Animals</title>
+    <style>
+        body { background-color: black; color: white; font-family: Arial, sans-serif; }
+        .animal { border: 1px solid white; padding: 10px; margin: 10px; }
+        img { max-width: 200px; height: auto; }
+    </style>
+</head>
+<body>
+    <h1>Brady's Farm Animals</h1>
+"""
+
+    # Loop through each animal in the list
+    for item in animals:
+        html_content += f"""
+    <div class="animal">
+        <img src="{item['image']}" alt="{item['name']}"><br>
+        <strong>Name:</strong> {item['name']}<br>
+        <strong>Breed:</strong> {item['breed']}<br>
+        <strong>Age:</strong> {item['age']}<br>
+        <strong>Pedigree:</strong> {item['pedigree']}<br>
+        <strong>Description:</strong> {item['description']}<br>
+    </div>
+    <hr>
+"""
+
+    # Close HTML tags
+    html_content += """
+</body>
+</html>
+"""
+
+    # Write to file
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(html_content)
+
+    print(f"HTML written to {output_path}")
     
 
 if __name__== '__main__':
